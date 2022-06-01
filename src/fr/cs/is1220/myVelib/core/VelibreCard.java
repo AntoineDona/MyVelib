f0c03d1f38a5ed68e58c8a0da5618f3e4bdf9ef1
Antoine Do Nascimento <antoinedn@live.fr>
2022-06-01 23:16:36 +0200

    fix: rename velibcard into velibre

---
+++ src/fr/cs/is1220/myVelib/core/VelibreCard.java
@@ -0,0 +1,14 @@
+package fr.cs.is1220.myVelib.core;
+
+public class VelibreCard extends Card {
+	
+	public VelibreCard() {
+		super();
+	}
+
+	public VelibreCard(Double timeBalance) {
+		super(timeBalance);
+		
+		// TODO Auto-generated constructor stub
+	}
+}
