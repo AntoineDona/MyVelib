96f106341a15c62c64e4705ef331b21178be9f26
Antoine Do Nascimento <antoinedn@live.fr>
2022-06-01 21:46:53 +0200

    restore stationfactory

--- src/fr/cs/is1220/myVelib/core/StationFactory.java
+++ src/fr/cs/is1220/myVelib/core/StationFactory.java
@@ -1,16 +1,57 @@
 package fr.cs.is1220.myVelib.core;
 
-public class StationFactory {
+import java.util.*;
 
-	public Station getStation(StationType stationType) {
-		if(stationType == null) {
+public class StationFactory extends AbstractFactory {
+	
+	@Override
+	public Station getStation(ArrayList<Object> props) {
+		if (props == null) {
 			return null;
+		} 
+		if (props.size() > 0 && props.get(0) instanceof StationType) { 		
+			//Si le 1e paramètre est bien de type Enum StationType
+			StationType stationType = (StationType)props.get(0);
+			
+			if(props.size() ==1 ) { 			
+				//Si c'est l'unique paramètre
+				
+				return new Station(stationType);
+				
+			} else if (props.size()>2 && props.get(1) instanceof Double && props.get(2) instanceof Double) { 
+				//Si on a des coordonnées GPS
+				
+				Double x = (Double) props.get(1);
+				Double y = (Double) props.get(2);
+				
+				if(props.size() == 3) {
+					//Si seulement 3 arguments
+					
+					return new Station(x, y, stationType);
+					
+				} else {
+					// On construit les HashMap après création de station, par défaut il sera vide!
+					return null;
+				}
+			}
 		}
-		else if(stationType.valueOf("Standard")) {
-			return new StandardStation();
-		}
-		else if(stationType.valueOf("Plus")) {
-			return new PlusStation();
-		}
+		return new Station(StationType.valueOf(""));
+	}
+	
+	@Override
+	public ParkingSlot getParkingSlot(ArrayList<Object> props){
+		return null;
+	}
+	@Override
+	public Bicycle getBicycle(ArrayList<Object> props){
+		return null;
+	}
+	@Override
+	public User getUser(ArrayList<Object> props){
+		return null;
+	}
+	@Override
+	public Card getCard(ArrayList<Object> props){
+		return null;
 	}
 }
