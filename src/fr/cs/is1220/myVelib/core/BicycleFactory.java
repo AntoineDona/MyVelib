64b5a1c526c3387a223e6236c38dfeeee0ef6931
Antoine Do Nascimento <antoinedn@live.fr>
2022-06-01 22:25:16 +0200

    add bicycle factory

--- src/fr/cs/is1220/myVelib/core/BicycleFactory.java
+++ src/fr/cs/is1220/myVelib/core/BicycleFactory.java
@@ -11,10 +11,25 @@ public class BicycleFactory extends AbstractFactory{
 	public ParkingSlot getParkingSlot(ArrayList<Object> props){
 		return null;
 	}
+	
 	@Override
 	public Bicycle getBicycle(ArrayList<Object> props){
-		return null;
+		if (props == null) {
+			return null;
+		}
+		if (props.size() == 0) {
+			System.out.println("Type of bicycle needed");
+			return null;
+		} else if (props.size() == 1 && BicycleType.contains((String) props.get(0))) {
+			//Si un argument de type String qui est contenu dans l'enum BicycleType
+			BicycleType bicycleType = BicycleType.valueOf((String) props.get(0));
+			return new Bicycle(bicycleType);
+		} else {
+			System.out.println("Too many arguments in getBicycle");
+			return null;
+		}
 	}
+	
 	@Override
 	public User getUser(ArrayList<Object> props){
 		return null;
