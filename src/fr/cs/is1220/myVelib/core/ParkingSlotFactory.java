e9d3f0f4cb2a320bc9cfb1e3a84f8b5bbc541649
Antoine Do Nascimento <antoinedn@live.fr>
2022-06-01 21:47:11 +0200

    rename props

--- src/fr/cs/is1220/myVelib/core/ParkingSlotFactory.java
+++ src/fr/cs/is1220/myVelib/core/ParkingSlotFactory.java
@@ -2,25 +2,25 @@ package fr.cs.is1220.myVelib.core;
 
 import java.util.ArrayList;
 
-public class ParkingSlotFactory {
+public class ParkingSlotFactory extends AbstractFactory{
 	@Override
-	public Station getStation(ArrayList<Object> params){
+	public Station getStation(ArrayList<Object> props){
 		return null;
 	}
 	@Override
-	public ParkingSlot getParkingSlot(ArrayList<Object> params){
+	public ParkingSlot getParkingSlot(ArrayList<Object> props){
 		return null;
 	}
 	@Override
-	public Bicycle getBicycle(ArrayList<Object> params){
+	public Bicycle getBicycle(ArrayList<Object> props){
 		return null;
 	}
 	@Override
-	public User getUser(ArrayList<Object> params){
+	public User getUser(ArrayList<Object> props){
 		return null;
 	}
 	@Override
-	public Card getCard(ArrayList<Object> params){
+	public Card getCard(ArrayList<Object> props){
 		return null;
 	}
 }
